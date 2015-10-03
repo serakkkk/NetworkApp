2016994bee053882dcc0f350ecdb92c4a8f7ae65
Fabio Bueno <fabiobueno13@hotmail.com>
2015-10-03 17:08:02 +0200

    Mejorar funcion ping personalizado
    Se añade casilla para guardar fichero con resultado,se programa alerta.
    
    Además se crean 2 funciones de envio de ping en funcion de si se marca o
    no la casilla.

--- unit2.pas
+++ unit2.pas
@@ -6,7 +6,7 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
-  ComCtrls;
+  ComCtrls, LCLType;
 
 type
 
@@ -20,7 +20,9 @@ type
     Label1: TLabel;
     Label2: TLabel;
     UpDown1: TUpDown;
-    procedure Edit2Change(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
+    procedure CheckBox1Click(Sender: TObject);
+
   private
     { private declarations }
   public
@@ -36,10 +38,47 @@ implementation
 
 { TForm2 }
 
-procedure TForm2.Edit2Change(Sender: TObject);
+///////////Proceso de aviso de marcar boton para guardar datos/////////////////
+procedure TForm2.CheckBox1Click(Sender: TObject);
+
+var  tipo: TMsgDlgType;
+     botones: TMsgDlgButtons;
+
 begin
 
+
+
+    tipo := mtWarning;
+    botones:= [mbOK];
+    MessageDlg ('Aviso', 'Si mandas muchos paquetes el fichero puede tardar en crearse y ralentizar el ordenador', tipo, botones,0);
+
 end;
+
+////////////////////////////////////////////////////////////////////////////////
+
+////////////Función ejecutar1/////////////////////////////
+function ejecutar1: string;
+begin
+  showmessage('No esta marcado');
+end;
+/////////////////////////////////////////////////////////////
+
+////////////Función ejecutar2/////////////////////////////
+function ejecutar2: string;
+begin
+  showmessage('Esta marcado');
+end;
+/////////////////////////////////////////////////////////////
+
+//////////VAMOS A DECLARAR LO QUE HARA EL BOTON REALIZAR PING //////////////////
+procedure TForm2.Button1Click(Sender: TObject);
+begin
+  If checkbox1.state=cbunchecked then ejecutar1
+  else ejecutar2;
+
+end;
+/////////////////////////////////////////////////////////////////
+
 
 end.
 
